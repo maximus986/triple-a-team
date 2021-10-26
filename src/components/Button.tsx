a84b5c0616879054568946ac139d05dfbec58329
Aleksandar Maksimovic <aleksandar@Aleksandars-MacBook-Pro.local>
2021-10-26 21:26:37 +0200

    Initial commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { Link } from './link';
+import { Link } from './Link';
 import { FunctionComponent } from 'react';
 
 interface ButtonProps {
