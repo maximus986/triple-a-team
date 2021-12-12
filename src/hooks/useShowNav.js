fc10d4feabf4281aefe19f5a966c9e962085d762
maximus986 <aleksandar.maksimovic2608@gmail.com>
2021-12-12 18:56:24 +0100

    Invert logic for displaying nav

--- src/hooks/useShowNav.js
+++ src/hooks/useShowNav.js
@@ -2,7 +2,5 @@ import { useLocation } from '@reach/router';
 
 export const useShowNav = () => {
   const location = useLocation();
-  return !(
-    location.pathname.includes('success') || location.pathname.includes('404')
-  );
+  return location.pathname === '/';
 };
